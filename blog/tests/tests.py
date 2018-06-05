8c49894c2bd6f7836ef3259bd79647ab7044e206
amakarudze <amakarudze@gmail.com>
2018-06-05 22:49:18 +0200

    Created tests module

--- blog/tests/tests.py
+++ blog/tests/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
 
-class HomePageTest(TestCase):
+class ViewsTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'blog/index.html')
+
