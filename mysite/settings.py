9baf0bf8d66171a2185ac27ae6a84c23616d861b
Anna Makarudze <amakarudze@gmail.com>
2021-07-24 15:46:16 +0200

    Add allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.makarudze.com', 'makarudze.com']
 
 SITE_ID = 1
 # Application definition
