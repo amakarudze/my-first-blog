3e6b1605f64579dc3ac631ce72d991387adb0a9c
Anna Makarudze <amakarudze@gmail.com>
2023-03-10 21:17:11 +0200

    Add blog namespace

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", include("blog.urls", namespace="blog")),
     path("comments/", include("django_comments.urls")),
     path("djrichtextfield/", include("djrichtextfield.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
